c2772c5956569f6ac0e73388ffca89c90c368e75
AgustinaDuarte22 <agustina.duarte@these.com.uy>
2023-05-23 10:01:48 -0300

    arreglo agus

--- spec/spec/features/car_spec.rb
+++ spec/spec/features/car_spec.rb
@@ -3,10 +3,10 @@ require 'capybara/rspec'
 
 RSpec.describe 'User authentication', type: :feature do
     describe 'when logged in' do
-        let(:car) { create(:user) }
+        let(:user) { create(:user) }
 
     before do
-     login_as(car, scope: :user)
+     login_as(user, scope: :user)
     end
 
     it 'displays the cars page' do
